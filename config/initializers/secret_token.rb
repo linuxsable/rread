9292d4f271655a207159ae64c3f04a4db0b245ef
Tyler Van Hoomissen <linuxsable@gmail.com>
2011-12-01 13:06:27 -0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newsie::Application.config.secret_token = '792cb24d8cd10440b13f72b2541d1cf4e9c8de5553101a8fc3d035d6f61216f1c3ec3dba6d31beab6b51cba2e056fb5b9ed6a16054d33d19188741c07873132f'
