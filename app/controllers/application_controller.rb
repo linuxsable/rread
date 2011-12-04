1e5d24cd501c44d44a2be37655603b901649e416
Tyler Van Hoomissen <linuxsable@gmail.com>
2011-12-04 15:34:25 -0800

    Small stuff.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,22 @@ class ApplicationController < ActionController::Base
   
   protected
   
+  # Get the current user
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
   end
-
-  def signed_in?
-    !!current_user
-  end
-
-  helper_method :current_user, :signed_in?
-
+  
+  # Assign current user and set session
   def current_user=(user)
     @current_user = user
     session[:user_id] = user.id
   end
+
+  # Is current user signed in
+  def signed_in?
+    !!current_user
+  end
+  
+  # Make these avail to the views as helpers
+  helper_method :current_user, :signed_in?
 end
