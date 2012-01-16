963afddae171fb04653a24980c9eefc0848d68ce
Tyler Van Hoomissen <linuxsable@gmail.com>
2012-01-16 14:41:21 -0800

    server setup stuff.

---
+++ config/deploy.rb
@@ -0,0 +1,32 @@
+set :application, "rread"
+
+#github stuff
+set :repository,  "git@github.com:iansilber/Newsie.git"
+set :scm, :git
+
+set :use_sudo,    false
+set :deploy_to,   "/rails_apps/#{application}"
+
+#server login
+set :user, "app"
+set :password, "Megaman1@"
+
+ssh_options[:forward_agent] = true
+
+# will be different entries for app, web, db if you host them on different servers
+server "50.56.208.142", :app, :web, :db, :primary => true
+
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+end
