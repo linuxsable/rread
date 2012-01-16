38e227dcef4c1e6ec88b6cb5de69a7a08ef84092
Tyler Van Hoomissen <linuxsable@gmail.com>
2012-01-16 09:18:04 -0800

    Static pages.

--- config/routes.rb
+++ config/routes.rb
@@ -55,6 +55,9 @@ Rread::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+
+  # Static pages
+  match ':action' => 'static#:action'
   
   resources :home, :user, :readers
   
