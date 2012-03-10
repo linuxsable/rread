3e91eee857648d0fd09e48f955f38d01ae0abf8c
Tyler Van Hoomissen <linuxsable@gmail.com>
2012-03-09 23:25:03 -0800

    Paperboy actually sends to queue now and logs

--- script/paperboy.rb
+++ script/paperboy.rb
@@ -1,20 +1,28 @@
 # PAPERBOY
 # WILL CHANGE YOUR LIFE
 # HE DELIVERS PAPER
-# JUST LIKE YOUR RETRO NES DREAMS
+# JUST LIKE IN YOUR RETRO NES DREAMS
 
-SLEEP_INTERVAL = 5
+# To run:
+#   RAILS_ENV=development rails runner script/paperboy.rb run
+
+SLEEP_INTERVAL = 30
 SYNC_INTERVAL = 5.minutes
 
 Daemons.run_proc('paperboy.rb') do
   # Setup the log
   log = ActiveSupport::BufferedLogger.new(File.join(Rails.root, "log", "paperboy.log"))
   Rails.logger = log
-  ActiveRecord::Base.logger = log
+  ActiveRecord::Base.logger = Logger.new('/dev/null')
+  # ActiveRecord::Base.logger = log
 
   loop {
     blogs = Blog.where("articles_last_syncd_at <= ?", Time.now - SYNC_INTERVAL)
-    p blogs.count
+    
+    blogs.each { |blog| blog.async_articles }
+
+    Rails.logger.info "Enqueing #{blogs.count} blogs to the BlogSyncer queue."
+
     sleep(SLEEP_INTERVAL)
   }
 end
